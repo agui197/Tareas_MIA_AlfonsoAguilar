d4b6a3624f6c069141f253205853c44f5a8c496a
Unknown <tripleaguilar@hotmail.com>
2018-11-06 18:12:41 -0600

    Plateamiento
    Separar segun energy source para cada estado y ver en que proporcion afecta del total la reduccion de uno

---
+++ CIFI 2018/solucion.m
@@ -0,0 +1,21 @@
+clc;
+clear all;
+close all;
+
+[num,text]=xlsread('annual_generation_state.xls');
+
+%%
+temp=cell2table(text);
+temp=temp(2:end,:);
+num=table(num(:,[1,5]));
+states=unique(temp(:,2));
+producers=unique(temp(:,3));
+energys=unique(temp(:,3));
+%%
+%num=num(:,[1,5])
+%%
+
+data=horzcat(temp(:,3),num(:,1));
+
+%%
+
