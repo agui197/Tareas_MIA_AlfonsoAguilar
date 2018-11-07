9dcb527d6b6df694f189fc1ce5026925031e66d9
Unknown <tripleaguilar@hotmail.com>
2018-11-06 18:34:07 -0600

    Actualizacion
    Filtracion de datos

--- CIFI 2018/solucion.m
+++ CIFI 2018/solucion.m
@@ -15,7 +15,8 @@ energys=unique(temp(:,3));
 %num=num(:,[1,5])
 %%
 
-data=horzcat(temp(:,3),num(:,1));
-
+data=horzcat(temp(:,[2,4]),num(:,1));
+    
 %%
-
+l=ismember(data(:,1),states(2,1),'rows')
+data(l,3)
