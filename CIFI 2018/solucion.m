ecb3deddf3e7266ab7ec2edb476e0c90ccbdb62d
Unknown <tripleaguilar@hotmail.com>
2018-11-08 18:11:23 -0600

    Actualizacion
    Adeline en matlab

--- CIFI 2018/solucion.m
+++ CIFI 2018/solucion.m
@@ -1,22 +1,35 @@
-clc;
-clear all;
-close all;
 
-[num,text]=xlsread('annual_generation_state.xls');
+%% Problema 3
+%datos=xlsread('Regresión.xlsx','Problema 3','A2:D201');
+%X1=datos(:,1);
+Y=xlsread('total_change_production.xlsx','Hoja1');
+%grado=2;
+%[Wmc,J]=adeline_MC(X,Y,grado)
+
+
 
 %%
-temp=cell2table(text);
-temp=temp(2:end,:); 
-num=table(num(:,[1,5]));
-states=unique(temp(:,2));
-producers=unique(temp(:,3));
-energys=unique(temp(:,3));
-%%
-%num=num(:,[1,5])
-%%
+% Gradiente Descendiente
+m=size(X1,1);
+Xa=ones(m,1);
 
-data=horzcat(temp(:,[2,4]),num(:,1));
-    
-%%
-l=ismember(data(:,1),states(2,1),'rows')
-data(l,3)
+X=[X1 X2 X3];
+for k=1:2
+    Xa=func_polinomio(X,k);
+end
+
+Wmc=inv(Xa'*Xa)*Xa'*Y;
+Wgd=rand(size(Xa,2),1); %Pesos iniciales
+eta=0.000000001 %Velocidad de convergencia
+
+for k=1:10000000
+    Yg_gd=Xa*Wgd; %Y estimada de gradiente descendiente
+    E=Y-Yg_gd;
+    J(k,1)=E'*E/(2*m);
+    dJdW=-E'*Xa/m;
+    Wgd=Wgd-eta*dJdW';
+end
+Yg_mc=Xa*Wmc;
+Yg_gd=Xa*Wgd;
+
+[Wmc Wgd]
