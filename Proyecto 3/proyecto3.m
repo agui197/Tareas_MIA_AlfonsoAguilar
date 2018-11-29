4f57a22927fba32a8e25132e07a5893b8fb263f7
Unknown <tripleaguilar@hotmail.com>
2018-11-29 15:32:23 -0600

    Actualizacion

--- Proyecto 3/proyecto3.m
+++ Proyecto 3/proyecto3.m
@@ -4,10 +4,10 @@ clc;
 
 data = xlsread('proyecto 3','DATASET1','A2:N1431');
 Ytrain=data(:,end)';
-Xtrain=data(:,5:9)';
+Xtrain=data(:,1:end-1)';
 datatest = xlsread('proyecto 3','PREDICCION','A2:N290');
 Y=datatest(:,end)';
-X=datatest(:,5:9)';
+X=datatest(:,1:end-1)';
 
 [Accu,Prec,Rec,red]=red_creation(1,Ytrain,Xtrain,Y,X);
 
@@ -30,3 +30,19 @@ while(mu<.8)
 end 
 [v,i]=max([table{:,5}])
 save tabla{i,4}
+
+%% recuperar las medidas del entrenamiento de la mejor red
+
+Yg=table{i,4}(Xtrain);
+Yg=round(Yg);
+
+TP=sum(Ytrain==1&Yg==1);
+TN=sum(Ytrain==0&Yg==0);
+FP=sum(Ytrain==0&Yg==1);
+FN=sum(Ytrain==1&Yg==0);
+
+Accu=(TP+TN)/(TP+TN+FP+FN);
+Prec=TP/(TP+FP);
+Rec=TP/(TP+FN);
+
+[Accu Prec Rec] 
